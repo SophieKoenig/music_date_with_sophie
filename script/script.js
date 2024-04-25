f038138e784fa56ea045f460f84977f756347d36
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2024-04-25 17:41:42 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,51 @@
+function populatePost(post) {
+  document.getElementById("postTitle").innerHTML = post.title;
+  var content = post.content;
+  document.getElementById("postContent").innerHTML = content;
+}
+
+function findQuery(param) {
+  console.log(param);
+  var urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+function createPreviewCard(card) {
+  var wrapper = document.getElementById("postsSummaries");
+  wrapper.innerHTML += `<li class="card-wrapper__card"><a href="./pages/post.html?id=${card.id}">
+    <img src="${card.previewImage}" alt="A random image" />
+    <div class="card-wrapper__content">
+    <h3>${card.title}</h3>
+    <p>${card.shortSummary}</p>
+    </div>
+    </a>
+    </li>`;
+}
+
+function getPosts() {
+  fetch("../data/posts.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      posts = data;
+      for (let i = 0; i < data.length; i++) {
+        createPreviewCard(data[i]);
+      }
+    });
+}
+
+function getPostFromId() {
+  var id = JSON.parse(findQuery("id"));
+  console.log("id", findQuery("id"));
+
+  fetch("../data/posts.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data);
+      for (let i = 0; i < data.length; i++) {
+        if (id === data[i].id) {
+          populatePost(data[i]);
+        }
+      }
+    });
+}
